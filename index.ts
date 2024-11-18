0038381b63616809251be0340223c15609afa177
bhatn <nagapooja.bhat@bahag.com>
2024-11-18 14:30:49 +0100

    test route

--- index.ts
+++ index.ts
@@ -1,24 +1,29 @@
-import express, {Request, Response, Application} from 'express'
+import express, { Request, Response, Application } from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import userRouter from './routes/userRoutes'
 import pool from './db/database'
 
-const app : Application = express()
+const app: Application = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 
 
-app.get('/', (req : Request, res : Response ) : any => {
+app.get('/', (req: Request, res: Response): any => {
     return res.send(`Welcome to our node and postgres API`)
 })
 
+app.get('/test', (req: Request, res: Response): any => {
+    return res.send(`Test route`)
+}
+)
+
 const startServer = async () => {
-    try{
+    try {
         // Test the connection to the database
         const client = await pool.connect();
         console.log("✅ Database connected successfully");
@@ -28,9 +33,9 @@ const startServer = async () => {
         // userRouter used after the connection succeed
         app.use('/api', userRouter)
         // we start the http server
-        app.listen(PORT, () =>  console.log(`Server running on port ${PORT}`))
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
     }
-    catch(err){
+    catch (err) {
         console.error("❌ Failed to connect to the database", err);
     }
 }
